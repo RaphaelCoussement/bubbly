6979214cfe698b731d385f182d91b958ad76f48d
raphou <raphael.coussement@diiage.org>
2025-03-29 00:07:41 +0100

    word suggestions

--- core/domain/src/main/java/org/raphou/bubbly/domain/word/IWordRepository.kt
+++ core/domain/src/main/java/org/raphou/bubbly/domain/word/IWordRepository.kt
@@ -2,5 +2,13 @@ package org.raphou.bubbly.domain.word
 
 interface IWordRepository {
     suspend fun initializeWords()
-    suspend fun getRandomWords(theme: String?): List<Word>
+    suspend fun getRandomWords(theme: String?, lobbyId: String): List<Word>
+    suspend fun addPlayerSuggestion(lobbyId: String, playerId: String, word: String)
+    suspend fun addWordSelected(lobbyId: String, word: String, isFound: Boolean, points: Int)
+    suspend fun getWordInfo(lobbyId: String, word: String): WordSelected?
+    suspend fun checkGameStatus(lobbyId: String): Boolean
+    suspend fun calculatePlayerScore(lobbyId: String, playerId: String): Int
+    suspend fun resetGame(lobbyId: String, playerId: String)
+    suspend fun getWordsToFind(lobbyId: String): List<String>
 }
+
--- features/game/src/main/java/org/raphou/bubbly/game/OtherPlayerScreenViewModel.kt
+++ features/game/src/main/java/org/raphou/bubbly/game/OtherPlayerScreenViewModel.kt
@@ -1,4 +1,71 @@
-package org.raphou.bubbly.game
+import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.raphou.bubbly.domain.word.IWordRepository
+import org.raphou.bubbly.domain.word.Word
+import org.raphou.bubbly.domain.word.WordSelected
+import java.lang.Exception
 
-class OtherPlayerScreenViewModel {
+class OtherPlayerScreenViewModel : ViewModel(), KoinComponent {
+
+    private val wordRepository: IWordRepository by inject()
+
+    private val _suggestions = mutableStateOf<List<String>>(emptyList())
+    val suggestions: State<List<String>> = _suggestions
+
+    private val _score = mutableStateOf(0)
+    val score: State<Int> = _score
+
+    private val _remainingSuggestions = mutableStateOf(10)
+    val remainingSuggestions: State<Int> = _remainingSuggestions
+
+    private val _isTimeUp = mutableStateOf(false)
+    val isTimeUp: State<Boolean> = _isTimeUp
+
+    private val _wordsToFind = mutableStateOf<List<String>>(emptyList())
+    val wordsToFind: State<List<String>> = _wordsToFind
+
+    fun addPlayerSuggestion(lobbyId: String, playerId: String, suggestedWord: String) {
+        viewModelScope.launch {
+            try {
+                if (_remainingSuggestions.value > 0) {
+                    Log.d("OtherPlayerScreenViewModel", "Adding word: $suggestedWord")
+                    wordRepository.addPlayerSuggestion(lobbyId, playerId, suggestedWord)
+                    _suggestions.value = _suggestions.value + suggestedWord
+                    _remainingSuggestions.value -= 1
+                    Log.d("OtherPlayerScreenViewModel", "Remaining suggestions: ${_remainingSuggestions.value}")
+                } else {
+                    Log.d("OtherPlayerScreenViewModel", "No remaining suggestions.")
+                    throw Exception("Nombre de suggestions atteint")
+                }
+            } catch (e: Exception) {
+                Log.e("OtherPlayerScreenViewModel", "Error adding suggestion: ${e.message}")
+            }
+        }
+    }
+
+    fun setTimeUp() {
+        _isTimeUp.value = true
+    }
+
+    fun resetGame(lobbyId: String, playerId: String) {
+        viewModelScope.launch {
+            try {
+                wordRepository.resetGame(lobbyId, playerId)
+                _suggestions.value = emptyList()
+                val finalScore = wordRepository.calculatePlayerScore(lobbyId, playerId)
+                _score.value = finalScore
+                val wordsToFindList = wordRepository.getWordsToFind(lobbyId)
+                _wordsToFind.value = wordsToFindList
+                _isTimeUp.value = true
+            } catch (e: Exception) {
+                Log.e("OtherPlayerScreenViewModel", "Error resetting game: ${e.message}")
+            }
+        }
+    }
 }
